99e4994ea31743ea2a8ba996f1d03ea7ea7e4527
wangjia184 <vcer@qq.com>
2018-10-15 20:22:01 +0800

    add basic test cases

--- MDBX.UnitTest/BasicTest.cs
+++ MDBX.UnitTest/BasicTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 using Xunit;
@@ -11,8 +12,72 @@ namespace MDBX.UnitTest
 {
     public class BasicTest
     {
-        [Fact(DisplayName = "put / set / delete single key (raw key)")]
+        [Fact(DisplayName = "put / set / delete single key (strong type)")]
         public void Test1()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "mdbx");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            using (MdbxEnvironment env = new MdbxEnvironment())
+            {
+                env.SetMaxDatabases(10) /* allow us to use a different db for testing */
+                   .Open(path, EnvironmentFlag.NoTLS, 0644);
+
+                DatabaseOption option = DatabaseOption.Create /* needed to create a new db if not exists */
+                    | DatabaseOption.IntegerKey/* opitimized for fixed key */;
+
+                // mdbx_put
+                using (MdbxTransaction tran = env.BeginTransaction())
+                {
+                    MdbxDatabase db = tran.OpenDatabase("basic_op_test", option);
+                    db.Put(10L, "ten");
+                    db.Put(1000L, "thousand");
+                    db.Put(1000000000L, "billion");
+                    db.Put(1000000L, "million");
+                    db.Put(100L, "hundred");
+                    db.Put(1L, "one");
+                    tran.Commit();
+                }
+
+
+                // mdbx_get
+                using (MdbxTransaction tran = env.BeginTransaction(TransactionOption.ReadOnly))
+                {
+                    MdbxDatabase db = tran.OpenDatabase("basic_op_test", option);
+
+                    string text = db.Get<long, string>(1000000L);
+                    Assert.NotNull(text);
+                    Assert.Equal("million", text);
+                }
+
+                // mdbx_del
+                using (MdbxTransaction tran = env.BeginTransaction())
+                {
+                    MdbxDatabase db = tran.OpenDatabase("basic_op_test", option);
+                    bool deleted = db.Del(100L);
+                    Assert.True(deleted);
+                    deleted = db.Del(100L);
+                    Assert.False(deleted);
+                    tran.Commit();
+                }
+
+
+                // mdbx_get
+                using (MdbxTransaction tran = env.BeginTransaction(TransactionOption.ReadOnly))
+                {
+                    MdbxDatabase db = tran.OpenDatabase("basic_op_test", option);
+
+                    string text = db.Get<long, string>(100L);
+                    Assert.Null(text);
+                }
+
+                env.Close();
+            }
+        }
+
+        [Fact(DisplayName = "put / set / delete single key (raw key)")]
+        public void Test2()
         {
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "mdbx");
             if (!Directory.Exists(path))
@@ -61,6 +126,48 @@ namespace MDBX.UnitTest
         }
 
         
+
+        [Fact(DisplayName = "put / set / delete single key (custom serializer)")]
+        public void Test3()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "mdbx");
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            // register serializer for our custom type
+            SerializerRegistry.Register(new BasicTest3PayloadSerializer());
+
+            using (MdbxEnvironment env = new MdbxEnvironment())
+            {
+                env.Open(path, EnvironmentFlag.NoTLS, 0644);
+
+                var putBytes = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+
+                // mdbx_put
+                using (MdbxTransaction tran = env.BeginTransaction())
+                {
+                    MdbxDatabase db = tran.OpenDatabase();
+                    db.Put("ana_key", new BasicTest3Payload() { Person = "Ana", Age = 50 } );
+                    tran.Commit();
+                }
+
+
+                // mdbx_get
+                using (MdbxTransaction tran = env.BeginTransaction(TransactionOption.ReadOnly))
+                {
+                    MdbxDatabase db = tran.OpenDatabase();
+
+                    BasicTest3Payload payload = db.Get<string, BasicTest3Payload>("ana_key");
+                    Assert.NotNull(payload);
+                    Assert.Equal("Ana", payload.Person);
+                    Assert.Equal(50, payload.Age);
+                }
+
+
+
+                env.Close();
+            }
+        }
 
 
     }
---
+++ MDBX.UnitTest/BasicTest3Payload.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace MDBX.UnitTest
+{
+    [DataContract]
+    public class BasicTest3Payload
+    {
+        [DataMember]
+        public string Person { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+    }
+
+
+    class BasicTest3PayloadSerializer : ISerializer<BasicTest3Payload>
+    {
+        public BasicTest3Payload Deserialize(byte[] buffer)
+        {
+            using (MemoryStream stream = new MemoryStream(buffer, false))
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(BasicTest3Payload));
+                return ser.ReadObject(stream) as BasicTest3Payload;
+            }
+        }
+
+        public byte[] Serialize(BasicTest3Payload payload)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(BasicTest3Payload));
+                ser.WriteObject(stream, payload);
+                stream.Flush();
+                stream.Position = 0;
+                return stream.ToArray();
+            }
+        }
+    }
+}
